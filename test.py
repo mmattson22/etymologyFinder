1c9d5016ebe69c5e2576c9f0d5ec81a93e116c2d
MGRiv <mgrivera501@gmail.com>
2015-11-14 22:28:20 -0500

    test api requests

--- test.py
+++ test.py
@@ -2,9 +2,29 @@ import urllib2
 import json
 
 #url = "https://www.google.com/#safe=off&q=iguana"
-url='http://www.dictionaryapi.com/api/v1/references/collegiate/xml/test?key=bfdd18d7-bf71-407d-bd66-430d3f0a89b7'
 
+url='http://api.wordnik.com:80/v4/word.json/Title/etymologies?useCanonical=true&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5'
+"""
 
 request = urllib2.urlopen(url)
 result = request.read()
+r = json.loads(result)
 print result
+print "\n"
+print r
+print "\n"
+print r.keys()
+
+url='http://api.openweathermap.org/data/2.5/weather?q="nyc"&appid=ecdb9f3fb43f5f8663867db2633c7638&units=imperial'
+"""
+
+request = urllib2.urlopen(url)
+result = request.read()
+r = json.loads(result)
+print result
+print
+print r
+print
+print r.keys()
+print r['main']
+print r['main']['temp']
