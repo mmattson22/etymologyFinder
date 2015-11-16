3285cd0fac0c2df2925c0fc6fb81cbe66ed1acd5
Michael Mattson <mhmattson@gmail.com>
2015-11-15 20:39:41 -0500

    this is bad

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import urllib2
 import json
+from json2html import *
 
 
 url='http://api.wordnik.com:80/v4/word.json/equal/etymologies?useCanonical=true&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5'
@@ -12,4 +13,4 @@ r = json.loads(result)
 print
 print r
 print
-print r[0]
+print json2html.convert(json = r)
